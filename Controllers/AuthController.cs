0e4eb2bb9e4f71ae932023e2120e387c668752f8
Bartosz Cłapiński <bartosz.clapinski@gmail.com>
2022-12-15 17:13:26 +0100

    Create password hash method added

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Security.Cryptography;
+using ASP_NET_Securing_JWT.Users;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ASP_NET_Securing_JWT.Controllers;
 
@@ -7,5 +9,20 @@ namespace ASP_NET_Securing_JWT.Controllers;
 [ApiController]
 public class AuthController : ControllerBase
 {
+    public static User user = new User();
+
+    private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+    {
+        using (var hmac = new HMACSHA512())
+        {
+            passwordSalt = hmac.Key;
+            passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        }
+    }
     
+    [HttpPost("register")]
+    public async Task<ActionResult<User>> RegisterUser(UserDto user)
+    {
+        
+    }
 }
