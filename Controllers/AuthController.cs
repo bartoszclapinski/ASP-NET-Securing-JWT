32584f221e0611d91cae9f59708f5920cbc0b79e
Bartosz Cłapiński <bartosz.clapinski@gmail.com>
2022-12-15 17:36:40 +0100

    Login and verify password.

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -19,10 +19,42 @@ public class AuthController : ControllerBase
             passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
         }
     }
+
+    private bool VerifyPassword(string password, byte[] passwordHash, byte[] passwordSalt)
+    {
+        using (var hmac = new HMACSHA512(passwordSalt))
+        {
+            var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            return computedHash.SequenceEqual(passwordHash);
+        }
+        
+    }
     
     [HttpPost("register")]
-    public async Task<ActionResult<User>> RegisterUser(UserDto user)
+    public async Task<ActionResult<User>> RegisterUser(UserDto newUser)
     {
-        
+        CreatePasswordHash(newUser.Password, out byte[] passwordHash, out byte[] passwordSalt);
+
+        user.Username = newUser.Username;
+        user.PasswordHash = passwordHash;
+        user.PasswordSalt = passwordSalt;
+
+        return Ok(user);
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<string>> Login(UserDto userlogin)
+    {
+        if (user.Username != userlogin.Username)
+        {
+            return BadRequest("Username not exists.");
+        }
+
+        if (!VerifyPassword(userlogin.Password, user.PasswordHash, user.PasswordSalt))
+        {
+            return BadRequest("Wrong password.");
+        }
+
+        return Ok("MY_TOKEN");
     }
 }
